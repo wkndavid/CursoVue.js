7edd3fc91a179c1b1cb4188d3027ab94a383f3e5
wkndavid <workingnown@gmail.com>
2022-02-14 12:57:39 -0300

    Hash History Remove

--- myApp/my-app/src/main.js
+++ myApp/my-app/src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from './views/Home'
 import Company from './views/Company'
 import Contact from './views/Contact'
@@ -13,7 +13,7 @@ const routes = [
 ]
 
 const router = createRouter({ 
-    history: createWebHashHistory(),
+    history: createWebHistory (),
     routes,
 })
 
