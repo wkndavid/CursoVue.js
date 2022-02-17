c4fbb6bed36087f15999911f452d70c342646496
wkndavid <workingnown@gmail.com>
2022-02-17 12:54:06 -0300

    Navegação Programática

--- myApp/my-app/src/views/Home.vue
+++ myApp/my-app/src/views/Home.vue
@@ -16,7 +16,12 @@ export default {
     },   
 
     created() {
-        console.log(this.$route);
+        // this.$router.push('/a-empresa')
+        // this.$router.push({ path: '/a-empresa'})
+        // this.$router.push({ name: 'company'})
+        // this.$router.push({ name: 'company', query: { curso: 'laravue'}})
+        // this.$router.push({ name: 'company', hash: '#laravue'})
+        this.$router.push({ name: 'team', params: { member: 'david-jesus' }})
         console.log(this.$router);
         },
 
