1a8cf489893132e7bea95b0f04195910c37f7600
wkndavid <workingnown@gmail.com>
2022-02-21 23:57:08 -0300

    Vuex Installation

---
+++ myApp/app2/src/main.js
@@ -0,0 +1,23 @@
+import { createApp } from 'vue'
+import { createStore } from 'vuex'
+import App from './App.vue'
+
+const store = createStore({
+    state(){
+        return{
+            first_name: 'David',
+            last_name: 'Jesus',
+            email: 'wkndavid@wkn.com'
+        }
+    },
+    mutation: {
+        updateName(){
+
+        },
+    },
+});
+
+createApp(App)
+.use(store)
+.mount('#app')
+
