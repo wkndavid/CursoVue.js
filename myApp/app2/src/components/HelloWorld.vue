9f8200eb7ac76f26d10ac683bcd968caa6928df2
wkndavid <workingnown@gmail.com>
2022-02-22 23:22:35 -0300

    Vuex State

--- myApp/app2/src/components/HelloWorld.vue
+++ myApp/app2/src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+        {{ fullName }} <br>
+        {{ $store.state.email }}
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -35,7 +37,12 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  computed: {
+    fullName(){
+    return ` ${ this.$store.state.first_name } ${ this.$store.state.last_name } `;
+    },
+  },
 }
 </script>
 
