5dbcef72a02d78ee30f235c18e1a85e63a5fdf15
wkndavid <workingnown@gmail.com>
2022-02-28 00:52:36 -0300

    mapGetters part1

--- myApp/app2/src/App.vue
+++ myApp/app2/src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div class="container mt-10">
+  <div class="container mt-5">
     {{ $store.getters.fullName }}
     <Counter/>
   </div>
 </template>
 
 <script>
-import Counter from './components/Counter.vue';
- 
+import Counter from './components/Counter.vue'
+import { mapGetters } from 'vuex'
+
 export default {
   name: 'App',
   components: {
     Counter,
   },
+  computed: {
+    ...mapGetters(['fullName'])
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
